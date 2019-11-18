91b5e8466b08e0f1ecc3fabcb8b5918889248a06
Bill Fly <billfly@programmer.net>
2019-11-18 17:57:33 -0600

    Asynchronous Access

--- src/test/java/edu/dcccd/reactive/JokeServiceTest.java
+++ src/test/java/edu/dcccd/reactive/JokeServiceTest.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,5 +24,23 @@ public class JokeServiceTest {
         String joke = service.getJokeSync("Craig", "Walls");
         logger.info("\n" + joke);
         assertTrue(joke.contains("Craig") || joke.contains("Walls"));
+    }
+
+    @Test
+    public void getJokeAsync() {
+        String joke = service.getJokeAsync("Craig", "Walls")
+            .block(Duration.ofSeconds(2));
+        logger.info(joke);
+        assertTrue(joke.contains("Craig") || joke.contains("Walls"));
+    }
+
+    @Test
+    public void useStepVerifier() {
+        StepVerifier.create(service.getJokeAsync("Craig", "Walls"))
+            .assertNext(joke -> {
+                logger.info(joke);
+                assertTrue(joke.contains("Craig") || joke.contains("Walls"));
+            })
+            .verifyComplete();
     }
 }
